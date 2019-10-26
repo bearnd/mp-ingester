595aac86e64bf2b05f5f1b99031f2f462fd7fd9a
Adamos Kyriakou <somada141@gmail.com>
2019-10-26 20:30:01 +1030

    [ch1701]: Added new `ParserXmlMedlineHealthTopic` class.
    [ch1701]: Added new `ParserXmlMedlineHealthTopic` class to parse `MedlinePlus Health Topic XML` files.

--- mp_ingester/parsers.py
+++ mp_ingester/parsers.py
@@ -7,8 +7,9 @@ containing the retrieved data.
 """
 
 import abc
+import datetime
 import gzip
-from typing import Union, Dict, Optional, Iterable, BinaryIO, Any
+from typing import List, Union, Dict, Optional, Iterable, BinaryIO, Any
 
 from lxml import etree
 
@@ -202,3 +203,409 @@ class ParserXmlMedlineGroups(ParserXmlBase):
                 continue
 
             yield health_topic_group
+
+
+class ParserXmlMedlineHealthTopic(ParserXmlBase):
+    """ XML parser class meant to parse `MedlinePlus Health Topic XML`
+        files.
+    """
+
+    def __init__(self, **kwargs):
+        """ Constructor."""
+
+        super(ParserXmlMedlineHealthTopic, self).__init__(kwargs=kwargs)
+
+    def parse_also_called(self, element: etree.Element) -> Dict:
+        """ Parses an element of type `also-called` and returns the values of
+            the contained elements.
+
+        Args:
+            element (etree.Element): The element of type `also-called`.
+
+        Returns:
+            Dict: The parsed values of the contained elements.
+        """
+
+        if element is None:
+            return {}
+
+        also_called = {"name": self._et(element=element)}
+
+        return also_called
+
+    def parse_also_calleds(
+        self, element_health_topic: etree.Element
+    ) -> List[Optional[Dict]]:
+        """ Extracts and parses `<also-called>` elements from a `<health-topic>`
+            element and returns the values of the contained elements.
+
+        Args:
+            element_health_topic (etree.Element): The `<health-topic>` element.
+
+        Returns:
+            List[Optional[Dict]]: The parsed values of the contained elements.
+        """
+
+        also_calleds = []
+
+        if element_health_topic is None:
+            return also_calleds
+
+        for element in element_health_topic.findall("also-called"):
+            also_calleds.append(self.parse_also_called(element=element))
+
+        return also_calleds
+
+    def parse_group(self, element: etree.Element) -> Dict:
+        """ Parses an element of type `group` and returns the values of the
+            contained elements.
+
+        Args:
+            element (etree.Element): The element of type `group`.
+
+        Returns:
+            Dict: The parsed values of the contained elements.
+        """
+
+        if element is None:
+            return {}
+
+        group = {
+            "id": int(self._eav(element=element, attribute="id")),
+            "url": self._eav(element=element, attribute="url"),
+            "name": self._et(element=element),
+        }
+
+        return group
+
+    def parse_groups(
+        self, element_health_topic: etree.Element
+    ) -> List[Optional[Dict]]:
+        """ Extracts and parses `<group>` elements from a `<health-topic>`
+            element and returns the values of the contained elements.
+
+        Args:
+            element_health_topic (etree.Element): The `<health-topic>` element.
+
+        Returns:
+            List[Optional[Dict]]: The parsed values of the contained elements.
+        """
+
+        groups = []
+
+        if element_health_topic is None:
+            return groups
+
+        for element in element_health_topic.findall("group"):
+            groups.append(self.parse_group(element=element))
+
+        return groups
+
+    def parse_descriptor(self, element: etree.Element) -> Dict:
+        """ Parses an element of type `descriptor` and returns the values of
+            the contained elements.
+
+        Args:
+            element (etree.Element): The element of type `descriptor`.
+
+        Returns:
+            Dict: The parsed values of the contained elements.
+        """
+
+        if element is None:
+            return {}
+
+        descriptor = {
+            "id": self._eav(element=element, attribute="id"),
+            "name": self._et(element=element),
+        }
+
+        return descriptor
+
+    def parse_qualifier(self, element: etree.Element) -> Dict:
+        """ Parses an element of type `qualifier` and returns the values of
+            the contained elements.
+
+        Args:
+            element (etree.Element): The element of type `qualifier`.
+
+        Returns:
+            Dict: The parsed values of the contained elements.
+        """
+
+        if element is None:
+            return {}
+
+        qualifier = {
+            "id": self._eav(element=element, attribute="id"),
+            "name": self._et(element=element),
+        }
+
+        return qualifier
+
+    def parse_qualifiers(
+        self, element_mesh_heading: etree.Element
+    ) -> List[Optional[Dict]]:
+        """ Extracts and parses `<qualifier>` elements from a `<mesh-heading>`
+            element and returns the values of the contained elements.
+
+        Args:
+            element_mesh_heading (etree.Element): The `<mesh-heading>` element.
+
+        Returns:
+            List[Optional[Dict]]: The parsed values of the contained elements.
+        """
+
+        qualifiers = []
+
+        if element_mesh_heading is None:
+            return qualifiers
+
+        for element in element_mesh_heading.findall("qualifier"):
+            qualifiers.append(self.parse_qualifier(element=element))
+
+        return qualifiers
+
+    def parse_mesh_heading(self, element: etree.Element) -> Dict:
+        """ Parses an element of type `mesh-heading` and returns the values of
+            the contained elements.
+
+        Args:
+            element (etree.Element): The element of type `mesh-heading`.
+
+        Returns:
+            Dict: The parsed values of the contained elements.
+        """
+
+        if element is None:
+            return {}
+
+        mesh_heading = {
+            "descriptor": self.parse_descriptor(
+                element=element.find("descriptor")
+            ),
+            "qualifiers": self.parse_qualifiers(element_mesh_heading=element),
+        }
+
+        return mesh_heading
+
+    def parse_mesh_headings(
+        self, element_health_topic: etree.Element
+    ) -> List[Optional[Dict]]:
+        """ Extracts and parses `<mesh-heading>` elements from a
+            `<health-topic>` element and returns the values of the contained
+            elements.
+
+        Args:
+            element_health_topic (etree.Element): The `<health-topic>` element.
+
+        Returns:
+            List[Optional[Dict]]: The parsed values of the contained elements.
+        """
+
+        mesh_headings = []
+
+        if element_health_topic is None:
+            return mesh_headings
+
+        for element in element_health_topic.findall("mesh-heading"):
+            mesh_headings.append(self.parse_mesh_heading(element=element))
+
+        return mesh_headings
+
+    def parse_primary_institute(self, element: etree.Element) -> Dict:
+        """ Parses an element of type `primary-institute` and returns the values
+            of the contained elements.
+
+        Args:
+            element (etree.Element): The element of type `primary-institute`.
+
+        Returns:
+            Dict: The parsed values of the contained elements.
+        """
+
+        if element is None:
+            return {}
+
+        primary_institute = {
+            "url": self._eav(element=element, attribute="url"),
+            "name": self._et(element=element),
+        }
+
+        return primary_institute
+
+    def parse_related_topic(self, element: etree.Element) -> Dict:
+        """ Parses an element of type `related-topic` and returns the values of
+            the contained elements.
+
+        Args:
+            element (etree.Element): The element of type `related-topic`.
+
+        Returns:
+            Dict: The parsed values of the contained elements.
+        """
+
+        if element is None:
+            return {}
+
+        related_topic = {
+            "url": self._eav(element=element, attribute="url"),
+            "id": self._eav(element=element, attribute="id"),
+            "name": self._et(element=element),
+        }
+
+        return related_topic
+
+    def parse_related_topics(
+        self, element_health_topic: etree.Element
+    ) -> List[Optional[Dict]]:
+        """ Extracts and parses `<related-topic>` elements from a
+            `<health-topic>` element and returns the values of the contained
+            elements.
+
+        Args:
+            element_health_topic (etree.Element): The `<health-topic>` element.
+
+        Returns:
+            List[Optional[Dict]]: The parsed values of the contained elements.
+        """
+
+        related_topics = []
+
+        if element_health_topic is None:
+            return related_topics
+
+        for element in element_health_topic.findall("related-topic"):
+            related_topics.append(self.parse_related_topic(element=element))
+
+        return related_topics
+
+    def parse_see_reference(self, element: etree.Element) -> Dict:
+        """ Parses an element of type `see-reference` and returns the values of
+            the contained elements.
+
+        Args:
+            element (etree.Element): The element of type `see-reference`.
+
+        Returns:
+            Dict: The parsed values of the contained elements.
+        """
+
+        if element is None:
+            return {}
+
+        see_reference = {"name": self._et(element=element)}
+
+        return see_reference
+
+    def parse_see_references(
+        self, element_health_topic: etree.Element
+    ) -> List[Optional[Dict]]:
+        """ Extracts and parses `<see-reference>` elements from a
+            `<health-topic>` element and returns the values of the contained
+            elements.
+
+        Args:
+            element_health_topic (etree.Element): The `<health-topic>` element.
+
+        Returns:
+            List[Optional[Dict]]: The parsed values of the contained elements.
+        """
+
+        see_references = []
+
+        if element_health_topic is None:
+            return see_references
+
+        for element in element_health_topic.findall("see-reference"):
+            see_references.append(self.parse_see_reference(element=element))
+
+        return see_references
+
+    def parse_health_topic(self, element: etree.Element) -> Dict:
+        """ Parses an element of type `health-topic` and returns the values of
+            the contained elements.
+
+        Args:
+            element (etree.Element): The element of type `health-topic`.
+
+        Returns:
+            Dict: The parsed values of the contained elements.
+        """
+
+        if element is None:
+            return {}
+
+        # Skip Spanish entities.
+        if self._eav(element=element, attribute="language") == "Spanish":
+            return {}
+
+        health_topic = {
+            "meta-desc": self._eav(element=element, attribute="meta-desc"),
+            "title": self._eav(element=element, attribute="title"),
+            "url": self._eav(element=element, attribute="url"),
+            "id": int(self._eav(element=element, attribute="id")),
+            "date-created": datetime.datetime.strptime(
+                self._eav(element=element, attribute="date-created"), "%m/%d/%Y"
+            ).date(),
+            "also_calleds": self.parse_also_calleds(
+                element_health_topic=element
+            ),
+            "full-summary": self._et(element.find("full-summary")),
+            "groups": self.parse_groups(element_health_topic=element),
+            # Skipping `<language-mapped-topic>` elements.
+            "mesh-headings": self.parse_mesh_headings(
+                element_health_topic=element
+            ),
+            # Skipping `<other-language>` elements.
+            "primary-institute": self.parse_primary_institute(
+                element=element.find("primary-institute")
+            ),
+            "related-topics": self.parse_related_topics(
+                element_health_topic=element
+            ),
+            "see-references": self.parse_see_references(
+                element_health_topic=element
+            )
+            # Skipping `<site>` elements.
+        }
+
+        return health_topic
+
+    def parse(self, filename_xml: str) -> Iterable[Dict]:
+        """ Parses a `MedlinePlus Health Topic XML` file and yields Python
+            dictionaries containing the data for each health-topic.
+
+        Args:
+            filename_xml (str): The path to the `MedlinePlus Health Topic XML`
+                file.
+
+        Returns:
+            Iterable[Dict]: An iterable yielding Python dictionaries containing
+                the data for each health-topic defined under the `MedlinePlus
+                Health Topic XML` file.
+        """
+
+        self.logger.info(
+            f"Parsing MedlinePlus Health Topic XML file '{filename_xml}'"
+        )
+
+        # Open the XML file.
+        file_xml = self.open_xml_file(filename_xml=filename_xml)
+
+        # Retrieve an iterable that yields `<health-topic>` XML elements from
+        # the XML file.
+        elements = self.generate_xml_elements(
+            file_xml=file_xml, element_tag="health-topic"
+        )
+
+        # Iterate over the `<health-topic>` elements and yield dictionaries with
+        # the parsed data.
+        for element in elements:
+            health_topic = self.parse_health_topic(element)
+
+            # Guard against empty documents.
+            if not health_topic:
+                continue
+
+            yield health_topic
