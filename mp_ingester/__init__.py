9ca2aecf3e00447a9ee415d26cb5731f4177b941
Adamos Kyriakou <somada141@gmail.com>
2019-10-28 10:59:58 +1030

    `black` reformatting.

--- mp_ingester/__init__.py
+++ mp_ingester/__init__.py
@@ -3,8 +3,8 @@
 """Top-level package for mp-ingester."""
 
 __author__ = """Adamos Kyriakou"""
-__email__ = 'somada141@gmail.com'
-__version__ = '0.1.0'
+__email__ = "somada141@gmail.com"
+__version__ = "0.1.0"
 
 from mp_ingester import config
 from mp_ingester import excs
